53e87941cb50fac2d91c310109bb350a131778f9
priyankas3110 <priyankasekarjaya@gmail.com>
2024-05-30 09:56:08 +0530

    four

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJI-ao6idNe4H4y_DcY7buMlTYlziJw4s",
-  authDomain: "reviv-93958.firebaseapp.com",
-  projectId: "reviv-93958",
-  storageBucket: "reviv-93958.appspot.com",
-  messagingSenderId: "164852211648",
-  appId: "1:164852211648:web:4e478337872e7006b3f225",
-  measurementId: "G-ETTEV0MFRT"
+  
 };
 
 // Initialize Firebase
